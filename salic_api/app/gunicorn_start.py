b8df31d7d6b2651460d2b46ecf8ab6e029bcdd62
Alexandre TK <alexandrekry@gmail.com>
2018-01-31 14:16:30 -0200

    Setting up gunicorn

---
+++ salic_api/app/gunicorn_start.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from .rate_limiting import limiter
+
+
+def create_app(config_file='development.cfg'):
+    from .urls import make_urls
+    from .general_config import ENVIRONMENT
+
+    app = Flask(__name__)
+    app.config.from_pyfile(ENVIRONMENT)
+
+    make_urls(app)
+    return app
+
+app = create_app()

c30b39ca445450c52a99a02cf7117b39ba3f71e6
Marcos Ronaldo <marcos.rpj2@gmail.com>
2018-04-04 10:06:41 -0300

    Removed unused code on fornecedor_count
      - total is being calculated by a generic method
    
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Marcos Ronaldo <marcos.rpj2@gmail.com>

--- salic_api/resources/fornecedor/query.py
+++ salic_api/resources/fornecedor/query.py
@@ -56,29 +56,6 @@ class FornecedorQuery(Query):
 
         return query
 
-    def count(self, cgccpf=None, PRONAC=None, nome=None):
-        if cgccpf is not None:
-            query = text(FORNECEDOR_COUNT_CGCCPF)
-            result = self.sql_connector.session.execute(
-                query, {'cgccpf': '%{}%'.format(cgccpf)}).fetchall()
-
-        elif nome is not None:
-            query = text(FORNECEDOR_COUNT_NOME)
-            result = self.sql_connector.session.execute(
-                query, {'nome': '%{}%'.format(nome)}).fetchall()
-
-        elif PRONAC is not None:
-            query = text(FORNECEDOR_COUNT_PRONAC)
-            result = self.sql_connector.session.execute(
-                query, {'PRONAC': PRONAC}).fetchall()
-
-        else:
-            query = text(FORNECEDOR_COUNT)
-            result = self.session.execute(query, {}).fetchall()
-
-        n_records = listify_queryset(result)
-        return n_records[0]['total']
-
 
 class ProductQuery(Query):
     """
@@ -127,71 +104,3 @@ class ProductQuery(Query):
                 )
 
         return query
-
-
-
-FORNECEDOR_COUNT_CGCCPF = """
-SELECT
-   COUNT(DISTINCT g.CNPJCPF) AS total
-
-   FROM BDCORPORATIVO.scSAC.tbComprovantePagamentoxPlanilhaAprovacao AS a
-   INNER JOIN BDCORPORATIVO.scSAC.tbComprovantePagamento AS b ON a.idComprovantePagamento = b.idComprovantePagamento
-   LEFT JOIN SAC.dbo.tbPlanilhaAprovacao AS PlanilhaAprovacao ON a.idPlanilhaAprovacao = PlanilhaAprovacao.idPlanilhaAprovacao
-   LEFT JOIN SAC.dbo.tbPlanilhaItens AS d ON PlanilhaAprovacao.idPlanilhaItem = d.idPlanilhaItens
-   LEFT JOIN Agentes.dbo.Nomes AS e ON b.idFornecedor = e.idAgente
-   LEFT JOIN BDCORPORATIVO.scCorp.tbArquivo AS f ON b.idArquivo = f.idArquivo
-   LEFT JOIN Agentes.dbo.Agentes AS g ON b.idFornecedor = g.idAgente
-   LEFT JOIN Agentes.dbo.Internet AS Internet ON b.idFornecedor = Internet.idAgente
-
-   WHERE (g.CNPJCPF LIKE :cgccpf) AND g.CNPJCPF IS NOT NULL
-"""
-
-FORNECEDOR_COUNT_NOME = """
-SELECT
-   COUNT(DISTINCT g.CNPJCPF) AS total
-
-   FROM BDCORPORATIVO.scSAC.tbComprovantePagamentoxPlanilhaAprovacao AS a
-   INNER JOIN BDCORPORATIVO.scSAC.tbComprovantePagamento AS b ON a.idComprovantePagamento = b.idComprovantePagamento
-   LEFT JOIN SAC.dbo.tbPlanilhaAprovacao AS PlanilhaAprovacao ON a.idPlanilhaAprovacao = PlanilhaAprovacao.idPlanilhaAprovacao
-   LEFT JOIN SAC.dbo.tbPlanilhaItens AS d ON PlanilhaAprovacao.idPlanilhaItem = d.idPlanilhaItens
-   LEFT JOIN Agentes.dbo.Nomes AS e ON b.idFornecedor = e.idAgente
-   LEFT JOIN BDCORPORATIVO.scCorp.tbArquivo AS f ON b.idArquivo = f.idArquivo
-   LEFT JOIN Agentes.dbo.Agentes AS g ON b.idFornecedor = g.idAgente
-   LEFT JOIN Agentes.dbo.Internet AS Internet ON b.idFornecedor = Internet.idAgente
-
-   WHERE (e.Descricao LIKE :nome) AND g.CNPJCPF IS NOT NULL
-"""
-
-FORNECEDOR_COUNT_PRONAC = """
-SELECT
-   COUNT(DISTINCT g.CNPJCPF) AS total
-
-   FROM BDCORPORATIVO.scSAC.tbComprovantePagamentoxPlanilhaAprovacao AS a
-   INNER JOIN BDCORPORATIVO.scSAC.tbComprovantePagamento AS b ON a.idComprovantePagamento = b.idComprovantePagamento
-   LEFT JOIN SAC.dbo.tbPlanilhaAprovacao AS PlanilhaAprovacao ON a.idPlanilhaAprovacao = PlanilhaAprovacao.idPlanilhaAprovacao
-   LEFT JOIN SAC.dbo.tbPlanilhaItens AS d ON PlanilhaAprovacao.idPlanilhaItem = d.idPlanilhaItens
-   LEFT JOIN Agentes.dbo.Nomes AS e ON b.idFornecedor = e.idAgente
-   LEFT JOIN BDCORPORATIVO.scCorp.tbArquivo AS f ON b.idArquivo = f.idArquivo
-   LEFT JOIN Agentes.dbo.Agentes AS g ON b.idFornecedor = g.idAgente
-   LEFT JOIN Agentes.dbo.Internet AS Internet ON b.idFornecedor = Internet.idAgente
-   JOIN SAC.dbo.Projetos AS Projetos ON PlanilhaAprovacao.idPronac = Projetos.IdPRONAC
-
-   WHERE (Projetos.AnoProjeto + Projetos.Sequencial = :PRONAC) AND g.CNPJCPF IS NOT NULL
-"""
-
-FORNECEDOR_COUNT = """
-SELECT
-   COUNT(DISTINCT g.CNPJCPF) AS total
-
-   FROM BDCORPORATIVO.scSAC.tbComprovantePagamentoxPlanilhaAprovacao AS a
-   INNER JOIN BDCORPORATIVO.scSAC.tbComprovantePagamento AS b ON a.idComprovantePagamento = b.idComprovantePagamento
-   LEFT JOIN SAC.dbo.tbPlanilhaAprovacao AS PlanilhaAprovacao ON a.idPlanilhaAprovacao = PlanilhaAprovacao.idPlanilhaAprovacao
-   LEFT JOIN SAC.dbo.tbPlanilhaItens AS d ON PlanilhaAprovacao.idPlanilhaItem = d.idPlanilhaItens
-   LEFT JOIN Agentes.dbo.Nomes AS e ON b.idFornecedor = e.idAgente
-   LEFT JOIN BDCORPORATIVO.scCorp.tbArquivo AS f ON b.idArquivo = f.idArquivo
-   LEFT JOIN Agentes.dbo.Agentes AS g ON b.idFornecedor = g.idAgente
-   LEFT JOIN Agentes.dbo.Internet AS Internet ON b.idFornecedor = Internet.idAgente
-   JOIN SAC.dbo.Projetos AS Projetos ON PlanilhaAprovacao.idPronac = Projetos.IdPRONAC
-
-   WHERE g.CNPJCPF IS NOT NULL
-"""

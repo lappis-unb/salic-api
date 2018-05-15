41ee1a254b11b9c9e3970b6bd0c6cb724dd949d3
Fabio Teixeira <fabio1079@gmail.com>
2018-05-15 09:50:55 -0300

    Add some tests for graphql_api

--- tests/test_graphql_api.py
+++ tests/test_graphql_api.py
@@ -1,22 +1,8 @@
 import pytest
-import re
 
 from salic_api.salic_graphql.schema import schema
-from .examples import INCENTIVADOR_RESPONSE
-
-INCENTIVADORES_QUERY = """
-    query {
-            incentivadores {
-            nome,
-            municipio,
-            UF,
-            responsavel,
-            cgccpf,
-            total_doado,
-            tipo_pessoa
-        }
-    }
-"""
+from .examples import (INCENTIVADOR_RESPONSE, FORNECEDOR_RESPONSE,
+                       PRODUTOS_RESPONSE, PROJETO_RESPONSE)
 
 
 def check_query_response(query, expected):
@@ -31,3 +17,65 @@ class TestGraphqlApi:
 
         check_query_response(incentivadores.data['incentivadores'][0],
                              INCENTIVADOR_RESPONSE)
+
+    def test_get_fornecedores(self):
+        fornecedores = schema.execute(FORNECEDORES_QUERY)
+
+        check_query_response(fornecedores.data['fornecedores'][0],
+                             FORNECEDOR_RESPONSE)
+
+    def test_get_fornecedor_produtos(self):
+        fornecedores_produtos = schema.execute(PRODUTOS_QUERY)
+
+        check_query_response(
+            fornecedores_produtos.data['fornecedores'][0]['produtos'][0],
+            PRODUTOS_RESPONSE['_embedded']['produtos'][0])
+
+
+INCENTIVADORES_QUERY = """
+    query {
+        incentivadores {
+            nome
+            municipio
+            UF
+            responsavel
+            cgccpf
+            total_doado
+            tipo_pessoa
+        }
+    }
+"""
+
+FORNECEDORES_QUERY = """
+    query {
+        fornecedores {
+            nome
+            email
+        }
+    }
+"""
+
+PRODUTOS_QUERY = """
+    query {
+        fornecedores(limit: 1) {
+            produtos {
+                id_planilha_aprovacao
+                justificativa
+                data_pagamento
+                nome
+                cgccpf
+                tipo_forma_pagamento
+                data_aprovacao
+                valor_pagamento
+                id_arquivo,
+                nr_comprovante
+                nome_fornecedor
+                id_comprovante_pagamento
+                tipo_documento
+                nr_documento_pagamento
+                nm_arquivo
+                PRONAC
+            }
+        }
+    }
+"""

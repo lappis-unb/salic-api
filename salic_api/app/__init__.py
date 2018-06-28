03052a4968faa9ba27791de32286e10d1adfcb32
Marcos Ronaldo <marcos.rpj2@gmail.com>
2018-06-28 11:42:22 -0300

    Allow access from everywhere

--- salic_api/app/__init__.py
+++ salic_api/app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from .rate_limiting import limiter
 
@@ -13,6 +14,8 @@ def create_app():
     from .general_config import ENVIRONMENT
 
     app = Flask(__name__, static_url_path=STATIC_URL_PATH)
+    CORS(app)
+
     app.config.from_pyfile(ENVIRONMENT)
 
     make_urls(app)
